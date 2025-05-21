09df570faf29dd341329d84a41a80211eb34d738
adams <adamskiptalam0@gmail.com>
2025-05-21 13:20:09 +0300

    Add rootfolder to user during registration

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,12 @@ export async function registerUser(req, res) {
 			return res.status(400).json({ message: "User already exists" });
 		const hashedPassword = await bcrypt.hash(password, 10);
 
-		const user = new User({ email, displayName, password: hashedPassword });
+		const user = new User({
+			email,
+			displayName,
+			password: hashedPassword,
+			rootFolder: null,
+		});
 		await user.save();
 		res.status(201).json({ message: "User registered successfully" });
 	} catch (err) {
