c4fd1f12e9cc95efa9ca762804a213578434f126
adams <adamskiptalam0@gmail.com>
2025-05-21 14:06:31 +0300

    add route to upload file to mongo and cloudinary

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,11 +1,19 @@
 import express from "express";
 import ensureAuth from "../middleware/auth-middleware.js";
+
+import uploadFileToCloudinaryAndDb from "../controllers/fileController.js";
 import upload from "../middleware/multer.js";
 
 const router = express.Router();
 
-router.post("/upload", ensureAuth, upload.single("file"), (req, res) => {
-	res.status(201).redirect("dashboard");
-});
+// upload file to database and cloudinary;
+router.post(
+	"/upload",
+	ensureAuth,
+	upload.single("file"),
+	uploadFileToCloudinaryAndDb
+);
+
+//delete file 
 
 export default router;
