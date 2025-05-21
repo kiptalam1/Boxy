0b7cd459ff1842adb1413a6ac1d065dbf6e11ade
adams <adamskiptalam0@gmail.com>
2025-05-21 15:56:10 +0300

    add try catchh to handle errors

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,10 +3,15 @@ import { registerUser } from "../controllers/authController.js";
 import ensureAuth from "../middleware/auth-middleware.js";
 import passport from "passport";
 import redirectIfAuthenticated from "../middleware/redirectAuthenticated.js";
+import { error } from "console";
 const router = express.Router();
 
 router.get("/login", redirectIfAuthenticated, (req, res) => {
-	res.render("login"); // or send login page
+	res.render("login", {
+		title: "Login in",
+		error: null,
+		values: {},
+	}); // or send login page
 });
 
 router.get("/register", redirectIfAuthenticated, (req, res) => {
@@ -15,15 +20,23 @@ router.get("/register", redirectIfAuthenticated, (req, res) => {
 
 router.post("/register", registerUser);
 router.post("/login", redirectIfAuthenticated, (req, res, next) => {
-	passport.authenticate("local", (err, user, info) => {
-		if (err) return next(err);
-		if (!user) return res.status(401).json({ message: info.message });
-
-		req.logIn(user, (err) => {
+	try {
+		passport.authenticate("local", (err, user, info) => {
 			if (err) return next(err);
-			return res.redirect("/dashboard");
+			if (!user) return res.status(401).json({ message: info.message });
+
+			req.logIn(user, (err) => {
+				if (err) return next(err);
+				return res.redirect("/dashboard");
+			});
+		})(req, res, next);
+	} catch (err) {
+		res.render("login", {
+			title: "Sign In",
+			error: err.message,
+			values: req.body, // Retain user input
 		});
-	})(req, res, next);
+	}
 });
 
 export default router;
